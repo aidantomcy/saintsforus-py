ef045489dcda47d7fa4d4a048c96d6d79cf0f710
Aidan Tomcy <aidantomcy@gmail.com>
2022-02-03 12:28:16 +0530

    Create feedback route and made form mail contents

--- website/views.py
+++ website/views.py
@@ -1,8 +1,40 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from dotenv import load_dotenv
+from os import getenv
+import smtplib
 
+
+load_dotenv()
 views = Blueprint("views", __name__, template_folder="templates")
 
 
 @views.route("/")
 def index():
     return render_template("index.html")
+
+
+@views.route("/feedback", methods=["GET", "POST"])
+def feedback():
+    name = request.form.get("name")
+    user_email = request.form.get("email")
+    message = request.form.get("message")
+
+    sender = "info.saintsforus@gmail.com"
+    receiver = "aidantomcy@gmail.com"
+    password = getenv("EMAIL_PASSWORD")
+    subject = "New form submission in Saints for Us website"
+    body = f"""
+There is a new form submission in the website, here are the details:
+
+Name: {name}
+Email: {user_email}
+Message: {message}
+"""
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+
+    server.login(sender, password)
+    server.sendmail(sender, receiver, body)
+
+    return render_template("feedback.html")
