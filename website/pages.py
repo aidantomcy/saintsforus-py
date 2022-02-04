5f958c5e84d0e038559cdab62245886ff3a91c19
Aidan Tomcy <aidantomcy@gmail.com>
2022-02-04 10:19:44 +0530

    Add second route

--- website/pages.py
+++ website/pages.py
@@ -2,7 +2,12 @@ from flask import Blueprint, render_template
 
 pages = Blueprint("pages", __name__, template_folder="templates")
 
+
 @pages.route("/stanthony")
 def stanthony():
     return render_template("stanthony.html")
 
+
+@pages.route("/stfrancisassisi")
+def stfrancisassisi():
+    return render_template("stfrancisassisi.html")
