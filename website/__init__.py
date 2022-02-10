1656aaf90b50653f449ab007f2133ff2447113c5
Aidan Tomcy <aidantomcy@gmail.com>
2022-02-10 18:32:46 +0530

    Add 500 error handler

--- website/__init__.py
+++ website/__init__.py
@@ -19,5 +19,9 @@ def create_app():
     @app.errorhandler(404)
     def page_not_found(e):
         return render_template("404.html")
+    
+    @app.errorhandler(500)
+    def server_error(e):
+        return render_template("500.html")
 
     return app
